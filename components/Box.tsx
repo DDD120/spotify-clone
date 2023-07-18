ef214ab74dd64309bb0b5e0ea7d93979eef2c5a4
DDD120 <dltnals9794@naver.com>
2023-07-18 22:53:11 +0900

    feat layout

---
+++ components/Box.tsx
@@ -0,0 +1,26 @@
+import { twMerge } from "tailwind-merge"
+
+interface Props {
+  children: React.ReactNode
+  className?: string
+}
+
+function Box({ children, className }: Props) {
+  return (
+    <div
+      className={twMerge(
+        `
+    bg-neutral-900
+      rounded-lg
+      h-fit
+      w-full
+    `,
+        className
+      )}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Box
